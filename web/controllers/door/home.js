d32ae13904ac4b869b0b93c926c66074b2ed5b18
Ruslan Banakh <ruslan@globalit.no>
2017-05-25 16:08:24 +0300

    WEB: db fix

--- web/controllers/door/home.js
+++ web/controllers/door/home.js
@@ -1,3 +1,4 @@
-module.exports = function(req, res) {
+module.exports = function(req, res, next) {
   res.render('home', {title: 'Door logs', message: 'Door logs'})
+  next()
 }
--- web/controllers/door/table.js
+++ web/controllers/door/table.js
@@ -1,6 +1,6 @@
 const db = require('../../db');
 
-module.exports = function(req, res) {
+module.exports = function(req, res, next) {
   db('door')
       .orderBy('created_at', 'desc')
       .then(result => {
