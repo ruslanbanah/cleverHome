f919b68fe1ef716ed4253e8b244b17932d85d042
Ruslan Banakh <ruslan.banakh@scoutgaminggroup.com>
2018-04-30 23:37:26 +0300

    fix bugs.

--- microservices/motionMessage/send.js
+++ microservices/motionMessage/send.js
@@ -1,14 +1,14 @@
-const TelegramBot = require('node-telegram-bot-api');
+const telegram = require('telegram-bot-api');
 const moment = require('moment')
 const fs = require('fs');
 
 const maxFiles = 1
 const patch = '/tmp/sata/'
 const _path = patch + moment().format('YYYY-MM-DD') + '/'
-const token = '251203343:AAEm-r5PujHF2sryLdEAcRnyBdvDoVAChDI';
 
- const bot = new TelegramBot(token);
-
+const api = new telegram({
+     token: '251203343:AAEm-r5PujHF2sryLdEAcRnyBdvDoVAChDI'
+ });
 
 fs.readdir(_path, (err, items)=>{
     if(err) {
@@ -17,17 +17,39 @@ fs.readdir(_path, (err, items)=>{
         return false
     }
     items.sort((a,b)=>(a<b) ? 1 : -1).slice(0, maxFiles).forEach((item) => {
-      sendPhoto(_path + item, 'File name : ' + item, 300810156) // my group
+      sendPhoto(_path + item, 'File name : ' + item, 300810156) //Home
     })
 })
+//
+// api.on('message', function(message)
+// {
+// 	var chat_id = message.chat.id;
+//
+// 	// It'd be good to check received message type here
+// 	// And react accordingly
+// 	// We consider that only text messages can be received here
+//
+// 	api.sendMessage({
+// 		chat_id: message.chat.id,
+// 		text: message.text ? message.text : 'This message doesn\'t contain text :('
+// 	})
+// 	.then(function(message)
+// 	{
+// 		console.log(message);
+// 	})
+// 	.catch(function(err)
+// 	{
+// 		console.log(err);
+// 	});
+// });
 
 function sendPhoto(name, message, chatId){
     if(name) {
-        return bot.sendPhoto({
+        return api.sendPhoto({
             chat_id : chatId,
             caption: message,
             photo: name
         })
     }
-    return bot.sendMessage(chatId, message)
+    return api.sendMessage(chatId, message)
 }
--- microservices/motionMessage/send_door.js
+++ microservices/motionMessage/send_door.js
@@ -1,14 +1,14 @@
-const TelegramBot = require('node-telegram-bot-api');
+const telegram = require('telegram-bot-api');
 const moment = require('moment')
 const fs = require('fs');
 
 const maxFiles = 1
-const patch = '/tmp/sata/'
+const patch = '/tmp/doorBell/'
 const _path = patch + moment().format('YYYY-MM-DD') + '/'
-const token = '251203343:AAEm-r5PujHF2sryLdEAcRnyBdvDoVAChDI';
 
- const bot = new TelegramBot(token);
-
+const api = new telegram({
+     token: '251203343:AAEm-r5PujHF2sryLdEAcRnyBdvDoVAChDI',
+ });
 
 fs.readdir(_path, (err, items)=>{
     if(err) {
@@ -17,35 +17,40 @@ fs.readdir(_path, (err, items)=>{
         return false
     }
     items.sort((a,b)=>(a<b) ? 1 : -1).slice(0, maxFiles).forEach((item) => {
-      sendPhoto(_path + item, 'File name : ' + item, 300810156) // my group
-      sendPhoto(_path + item, 'File name : ' + item, -189280924) //DoorBell Group
+      sendPhoto(_path + item, 'File name : ' + item, 300810156) //Home
+      sendPhoto(_path + item, 'File name : ' + item, -189280924) //DoorBell
     })
 })
-
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1] + ' ' + chatId; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
-//   console.log('chatId:', msg.chat)
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, 'Received your message: ' + chatId);
+//
+// api.on('message', function(message)
+// {
+// 	var chat_id = message.chat.id;
+//
+// 	// It'd be good to check received message type here
+// 	// And react accordingly
+// 	// We consider that only text messages can be received here
+//
+// 	api.sendMessage({
+// 		chat_id: message.chat.id,
+// 		text: message.text ? message.text : 'This message doesn\'t contain text :('
+// 	})
+// 	.then(function(message)
+// 	{
+// 		console.log(message);
+// 	})
+// 	.catch(function(err)
+// 	{
+// 		console.log(err);
+// 	});
 // });
+
 function sendPhoto(name, message, chatId){
     if(name) {
-        return bot.sendPhoto({
+        return api.sendPhoto({
             chat_id : chatId,
             caption: message,
             photo: name
         })
     }
-    return bot.sendMessage(chatId, message)
+    return api.sendMessage(chatId, message)
 }
